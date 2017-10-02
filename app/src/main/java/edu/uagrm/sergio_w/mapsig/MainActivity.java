0d35e13189a12d5b4144bf72c669b0625a329686
sergio <ser61_ol@yahoo.es>
2017-10-01 22:04:10 -0400

    antes de eliminar mapsactivity

--- app/src/main/java/edu/uagrm/sergio_w/mapsig/MainActivity.java
+++ app/src/main/java/edu/uagrm/sergio_w/mapsig/MainActivity.java
@@ -92,35 +92,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     locate = new ProgressDialog(MainActivity.this);
                     locate.setIndeterminate(true);
                     locate.setCancelable(false);
-                    locate.setMessage("Getting Location...");
+                    locate.setMessage("Obteniendo ubicacion...");
                     locate.show();
                 }
             }
         });
 
 
-        /*DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);*/
     }
-
-    /*@Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-        if (status == false)
-            super.onBackPressed();
-        else
-            moveTaskToBack(true);
-    }*/
 
     @Override
     protected void onResume() {
@@ -140,53 +120,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         if (status == true)
             unbindService();
     }
-
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.principal, menu);
-        return true;
-    }*/
-
-    /*@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
-
-   /* @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }*/
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
